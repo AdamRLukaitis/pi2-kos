1ad7f5ca8833f478bad7b0382f86946e7d4f993e
Kellerman Rivero <krsloco@gmail.com>
2015-12-15 07:06:16 -0430

    First version based on the osdev example working for pi2

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 CFLAGS="-O2 -mfpu=vfp -mfloat-abi=hard -march=armv7-a -mtune=cortex-a7"
-../toolchain/bin/arm-none-eabi-gcc -O2 -mfpu=vfp -mfloat-abi=hard -march=armv7-a -mtune=cortex-a7 -ffreestanding -nostdlib -c boot.S -o boot.o 
-../toolchain/bin/arm-none-eabi-gcc -O2 -mfpu=vfp -mfloat-abi=hard -march=armv7-a -mtune=cortex-a7 -ffreestanding -nostdlib -c test.c -o kernel.o
+
+../toolchain/bin/arm-none-eabi-gcc -march=armv7-a -fpic -ffreestanding -c boot.S -o boot.o 
+../toolchain/bin/arm-none-eabi-gcc -O2 -mfpu=vfp -mfloat-abi=hard -march=armv7-a -fpic -mtune=cortex-a7 -ffreestanding -std=gnu99 -c test.c -o kernel.o
 ../toolchain/bin/arm-none-eabi-gcc -O2 -mfpu=vfp -mfloat-abi=hard -march=armv7-a -mtune=cortex-a7 -ffreestanding -nostdlib -T linker.ld -o kos.elf boot.o kernel.o
+../toolchain/bin/arm-none-eabi-objcopy kos.elf -O binary kernel.img
